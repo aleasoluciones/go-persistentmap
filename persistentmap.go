f443442850d418b6b61e5d2867cfcb96804fb200
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-07-06 01:05:52 +0200

    Initial implementation (Not tested)

--- persistentmap.go
+++ persistentmap.go
@@ -7,42 +7,98 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-func main() {
-	db, err := bolt.Open("my.db", 0600, nil)
+type PersistentMap struct {
+	db   *bolt.DB
+	name string
+}
+
+func NewPersistentMap(name, basepath string) *PersistentMap {
+	db, err := bolt.Open(basepath+name+".db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("MyBucket"))
+		_, err := tx.CreateBucket([]byte(name))
 		if err != nil {
 			return fmt.Errorf("create bucket: %s", err)
 		}
 		return nil
 	})
 
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MyBucket"))
-		err := b.Put([]byte("answer"), []byte("42"))
+	return &PersistentMap{db: db, name: name}
+}
+
+func (m *PersistentMap) Set(key string, data []byte) {
+	m.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(m.name))
+		err := b.Put([]byte(key), data)
 		return err
 	})
+}
 
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MyBucket"))
-		v := b.Get([]byte("answer"))
-		fmt.Printf("The answer is: %s\n", v)
+func (m *PersistentMap) Get(key string) []byte {
+	returnValue := []byte{}
+	m.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(m.name))
+		returnValue = b.Get([]byte(key))
 		return nil
 	})
+	return returnValue
+}
 
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MyBucket"))
-		c := b.Cursor()
+func (m *PersistentMap) Close() {
+	m.db.Close()
+}
 
-		for k, v := c.First(); k != nil; k, v = c.Next() {
-			fmt.Printf("key=%s, value=%s\n", k, v)
-		}
-
-		return nil
+func (m *PersistentMap) Delete(key string) {
+	m.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(m.name))
+		err := b.Delete([]byte(key))
+		return err
 	})
+}
+
+type Tuple struct {
+	Key   string
+	Value []byte
+}
+
+func (m *PersistentMap) IterationChannel() chan Tuple {
+	returnChan := make(chan Tuple)
+
+	go func() {
+		m.db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(m.name))
+			c := b.Cursor()
+
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				returnChan <- Tuple{string(k), v}
+			}
+			close(returnChan)
+			return nil
+		})
+	}()
+
+	return returnChan
+}
+
+func main() {
+
+	persistentmap := NewPersistentMap("test", "./")
+	fmt.Println(persistentmap)
+
+	persistentmap.Set("answer1", []byte("42"))
+	persistentmap.Set("answer2", []byte("43"))
+	persistentmap.Set("answer3", []byte("44"))
+
+	for tuple := range persistentmap.IterationChannel() {
+		fmt.Println("Tuple", tuple.Key, string(tuple.Value))
+	}
+
+	fmt.Println("E1", string(persistentmap.Get("answer1")))
+	persistentmap.Delete("answer1")
+
+	fmt.Println("E2", string(persistentmap.Get("answer1")))
+
 }
