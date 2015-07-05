e7bdb11e5bfb4bea7056af8544c9bb6782f680dd
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-07-05 16:26:58 +0200

    Spike using bolt

---
+++ persistentmap.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+func main() {
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte("MyBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		err := b.Put([]byte("answer"), []byte("42"))
+		return err
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		v := b.Get([]byte("answer"))
+		fmt.Printf("The answer is: %s\n", v)
+		return nil
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+		}
+
+		return nil
+	})
+}
