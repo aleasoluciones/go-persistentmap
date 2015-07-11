588a745ac2592755013a200fef4660fc2a49302d
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-07-12 01:02:48 +0200

    Testing raw map iteration

--- persistentmap_test.go
+++ persistentmap_test.go
@@ -13,7 +13,7 @@ import (
 func TestRawBytesMap(t *testing.T) {
 	t.Parallel()
 
-	m := NewPersistentMap("rawmap.db")
+	m := NewPersistentMap("rawmap1.db")
 	fmt.Println(m)
 
 	m.Set("k1", []byte("value1"))
@@ -21,4 +21,21 @@ func TestRawBytesMap(t *testing.T) {
 
 	assert.Equal(t, m.Get("k1"), []byte("value1"))
 	assert.Equal(t, m.Get("k2"), []byte("value2"))
+}
+
+func TestRawBytesMapIteration(t *testing.T) {
+	t.Parallel()
+
+	m := NewPersistentMap("rawmap2.db")
+	fmt.Println(m)
+
+	m.Set("k1", []byte("value1"))
+	m.Set("k2", []byte("value2"))
+
+	c := m.IterationChannel()
+	fmt.Println("EFA1")
+
+	assert.Equal(t, <-c, Tuple{"k1", []byte("value1")})
+	fmt.Println("EFA2")
+	assert.Equal(t, <-c, Tuple{"k2", []byte("value2")})
 }
